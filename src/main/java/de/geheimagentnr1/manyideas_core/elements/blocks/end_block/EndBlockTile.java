e4b7d39c8dd8ed7330a3521b12807ead3d7812fb
GeheimagentNr1 <GeheimeLache@hotmail.com>
2019-12-09 12:07:12 +0100

    EndBlock wird jetzt korrekt gerendert

--- src/main/java/de/geheimagentnr1/manyideas_core/elements/blocks/end_block/EndBlockTile.java
+++ src/main/java/de/geheimagentnr1/manyideas_core/elements/blocks/end_block/EndBlockTile.java
@@ -1,11 +1,13 @@
 package de.geheimagentnr1.manyideas_core.elements.blocks.end_block;
 
 import de.geheimagentnr1.manyideas_core.elements.blocks.ModBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 
 
 public class EndBlockTile extends TileEntity {
@@ -22,8 +24,10 @@ public class EndBlockTile extends TileEntity {
 		if( world == null ) {
 			return true;
 		}
-		BlockState state = world.getBlockState( pos.offset( direction ) );
-		return state.getRenderType() == BlockRenderType.INVISIBLE ||
-			state.getBlock().getRenderLayer() != BlockRenderLayer.SOLID;
+		BlockPos direction_pos = pos.offset( direction );
+		BlockState direction_state = world.getBlockState( direction_pos );
+		return direction_state.getRenderType() == BlockRenderType.INVISIBLE ||
+			direction_state.getBlock().getRenderLayer() != BlockRenderLayer.SOLID &&
+				!Block.hasSolidSide( direction_state, world, direction_pos, direction );
 	}
 }
