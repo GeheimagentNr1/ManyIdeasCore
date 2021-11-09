464831fe790d848f7df7352c43747df8cd22400a
GeheimagentNr1 <geheimelache@hotmail.com>
2021-11-09 22:05:08 +0100

    Ported fixes to 1.17.1

--- src/main/java/de/geheimagentnr1/manyideas_core/elements/blocks/end_block/EndBlockEntity.java
+++ src/main/java/de/geheimagentnr1/manyideas_core/elements/blocks/end_block/EndBlockEntity.java
@@ -3,6 +3,7 @@ package de.geheimagentnr1.manyideas_core.elements.blocks.end_block;
 import de.geheimagentnr1.manyideas_core.elements.blocks.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -29,16 +30,19 @@ public class EndBlockEntity extends TheEndPortalBlockEntity {
 	@Override
 	public boolean shouldRenderFace( @Nonnull Direction face ) {
 		
+		return shouldRender( level, worldPosition, face );
+	}
+	
+	@SuppressWarnings( "WeakerAccess" )
+	public static boolean shouldRender( Level level, BlockPos pos, Direction face ) {
+		
 		if( level == null ) {
 			return true;
 		}
-		BlockPos direction_pos = worldPosition.relative( face );
+		BlockPos direction_pos = pos.relative( face );
 		BlockState direction_state = level.getBlockState( direction_pos );
 		if( direction_state.getRenderShape() == RenderShape.INVISIBLE ) {
 			return true;
-		}
-		if( direction_state.getBlock() instanceof IEndBlock ) {
-			return false;
 		}
 		return !Block.isFaceFull( direction_state.getShape( level, direction_pos ), face.getOpposite() );
 	}
